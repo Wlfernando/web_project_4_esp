ef41ab897dbd84c100edcc7d5449d88fc4fee23b
Fernando Alcázar Malfavón <4maferal@gmail.com>
2023-05-18 17:04:14 -0600

    Style: add classes on modal box

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,24 @@
-let cards = document.querySelector(".cards")
-let image = cards.querySelectorAll(".card__image")
-let imageTitle = cards.querySelectorAll(".card__place-name")
+const profile = document.querySelector(".profile");
+const editBtn = profile.querySelector(".profile__edit-button");
+const popUpActive = profile.querySelectorAll(".popup");
+const modalEditProfile = popUpActive[0];
+const addBtn = profile.querySelector(".profile__add-button");
+const modalAddImg = popUpActive[1];
+const closeBtn = profile.querySelectorAll(".modal-box__close-btn");
+
+editBtn.addEventListener("click", function openProfileEdit () {
+  modalEditProfile.classList.remove("popup_active")
+});
+
+closeBtn[0].addEventListener("click", function closeProfileEdit () {
+  modalEditProfile.classList.add("popup_active")
+});
+
+addBtn.addEventListener("click", function openImageAdd () {
+  modalAddImg.classList.remove("popup_active")
+});
+
+closeBtn[1].addEventListener("click", function closeImageAdd () {
+  modalAddImg.classList.add("popup_active")
+});
 
