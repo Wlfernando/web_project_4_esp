1ed353b32efa6d8b65ce5cfa1ce148b4e23cf91a
Fernando Alcázar Malfavón <4maferal@gmail.com>
2023-05-18 23:42:48 -0600

    feat: add tag figure for add image

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ const editBtn = profile.querySelector(".profile__edit-button");
 const popUpActive = profile.querySelectorAll(".popup");
 const modalEditProfile = popUpActive[0];
 const addBtn = profile.querySelector(".profile__add-button");
-const modalAddImg = popUpActive[1];
+const modalUploadImg = popUpActive[1];
 const closeBtn = profile.querySelectorAll(".modal-box__close-btn");
 const inputProfileName = popUpActive[0].querySelector('[name="profileName"]');
 const pageProfileName = profile.querySelector('.profile__user-name');
@@ -19,16 +19,17 @@ closeBtn[0].addEventListener("click", function closeProfileEdit () {
   modalEditProfile.classList.add("popup_active")
 });
 
-addBtn.addEventListener("click", function openImageAdd () {
-  modalAddImg.classList.remove("popup_active")
-});
-
-closeBtn[1].addEventListener("click", function closeImageAdd () {
-  modalAddImg.classList.add("popup_active")
-});
-
 saveBtn.addEventListener('click', function editProfile() {
   pageProfileName.textContent = inputProfileName.value;
   pageAboutMe.textContent = inputAboutMe.value;
   modalEditProfile.classList.add("popup_active")
 });
+
+addBtn.addEventListener("click", function openImageAdd () {
+  modalUploadImg.classList.remove("popup_active")
+});
+
+closeBtn[1].addEventListener("click", function closeImageAdd () {
+  modalUploadImg.classList.add("popup_active")
+});
+
