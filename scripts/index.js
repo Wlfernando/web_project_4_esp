2fd59d12f7cb0bdc942847b16f8da0cafd85cb9a
Fernando Alcázar Malfavón <4maferal@gmail.com>
2023-05-18 18:35:32 -0600

    Feat: work button edit

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,11 @@ const modalEditProfile = popUpActive[0];
 const addBtn = profile.querySelector(".profile__add-button");
 const modalAddImg = popUpActive[1];
 const closeBtn = profile.querySelectorAll(".modal-box__close-btn");
+const inputProfileName = popUpActive[0].querySelector('[name="profileName"]');
+const pageProfileName = profile.querySelector('.profile__user-name');
+const inputAboutMe = popUpActive[0].querySelector('[name="aboutMe"]');
+const pageAboutMe = profile.querySelector('.profile__about-me');
+const saveBtn = popUpActive[0].querySelector('.modal-box__save-btn')
 
 editBtn.addEventListener("click", function openProfileEdit () {
   modalEditProfile.classList.remove("popup_active")
@@ -22,3 +27,8 @@ closeBtn[1].addEventListener("click", function closeImageAdd () {
   modalAddImg.classList.add("popup_active")
 });
 
+saveBtn.addEventListener('click', function editProfile() {
+  pageProfileName.textContent = inputProfileName.value;
+  pageAboutMe.textContent = inputAboutMe.value;
+  modalEditProfile.classList.add("popup_active")
+});
