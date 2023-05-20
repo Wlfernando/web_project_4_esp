ba8ea8986603e1f9e6c81f88f31eec9958bef806
Fernando Alcázar Malfavón <4maferal@gmail.com>
2023-05-20 15:11:27 -0600

    fix: button submit

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const profile = document.querySelector(".profile");
-const editBtn = profile.querySelector(".profile__edit-button");
+const editProfileBtn = profile.querySelector(".profile__edit-button");
 const popUpActive = profile.querySelectorAll(".popup");
-const modalEditProfile = popUpActive[0];
+const popUpProfileForm = popUpActive[0];
 const addBtn = profile.querySelector(".profile__add-button");
 const modalUploadImg = popUpActive[1];
 const closeBtn = profile.querySelectorAll(".modal-box__close-btn");
@@ -9,27 +9,46 @@ const inputProfileName = popUpActive[0].querySelector('[name="profileName"]');
 const pageProfileName = profile.querySelector('.profile__user-name');
 const inputAboutMe = popUpActive[0].querySelector('[name="aboutMe"]');
 const pageAboutMe = profile.querySelector('.profile__about-me');
-const saveBtn = popUpActive[0].querySelector('.modal-box__save-btn')
+const saveBtn = popUpActive[0].querySelector('.modal-box__save-btn');
+const formElement = popUpActive[0].querySelector('.profile-form');
 
-editBtn.addEventListener("click", function openProfileEdit () {
-  modalEditProfile.classList.remove("popup_active")
+editProfileBtn.addEventListener("click", function openProfileEdit () {
+  popUpProfileForm.classList.add("popup_active")
 });
 
 closeBtn[0].addEventListener("click", function closeProfileEdit () {
-  modalEditProfile.classList.add("popup_active")
+  popUpProfileForm.classList.remove("popup_active")
 });
 
-saveBtn.addEventListener('click', function editProfile() {
+// function handleProfileFormSubmit(evt) {
+//   evt.preventDefault();
+//   pageProfileName.textContent = inputProfileName.value;
+//   pageAboutMe.textContent = inputAboutMe.value;
+//   popUpProfileForm.classList.remove("popup_active")
+// }
+
+// saveBtn.addEventListener('submit', handleProfileFormSubmit);
+formElement.addEventListener('submit', handleProfileFormSubmit);
+
+function handleProfileFormSubmit(event){
+  event.preventDefault();
   pageProfileName.textContent = inputProfileName.value;
   pageAboutMe.textContent = inputAboutMe.value;
-  modalEditProfile.classList.add("popup_active")
-});
+  popUpProfileForm.classList.remove("popup_active")
+};
+
+// handleProfileFormSubmit();
+
+// saveBtn.addEventListener('click', function editProfile() {
+//   pageProfileName.textContent = inputProfileName.value;
+//   pageAboutMe.textContent = inputAboutMe.value;
+//   PopUpProfileForm.classList.remove("popup_active")
+// });
 
 addBtn.addEventListener("click", function openImageAdd () {
-  modalUploadImg.classList.remove("popup_active")
-});
-
-closeBtn[1].addEventListener("click", function closeImageAdd () {
   modalUploadImg.classList.add("popup_active")
 });
 
+closeBtn[1].addEventListener("click", function closeImageAdd () {
+  modalUploadImg.classList.remove("popup_active")
+});
