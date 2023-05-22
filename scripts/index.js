024ac94d828706b7f68c40ec78cead703a1eaf4f
Fernando Alcázar Malfavón <4maferal@gmail.com>
2023-05-22 16:43:25 -0600

    Refactor: Change class names for modal to popup

--- scripts/index.js
+++ scripts/index.js
@@ -4,12 +4,12 @@ const popUpActive = profile.querySelectorAll(".popup");
 const popUpProfileForm = popUpActive[0];
 const addBtn = profile.querySelector(".profile__add-button");
 const inputImg = popUpActive[1];
-const closeBtn = profile.querySelectorAll(".modal-box__close-btn");
+const closeBtn = profile.querySelectorAll(".popup__close-btn");
 const inputProfileName = popUpActive[0].querySelector('[name="profileName"]');
 const pageProfileName = profile.querySelector('.profile__user-name');
 const inputAboutMe = popUpActive[0].querySelector('[name="aboutMe"]');
 const pageAboutMe = profile.querySelector('.profile__about-me');
-const saveBtn = popUpActive[0].querySelector('.modal-box__save-btn');
+const saveBtn = popUpActive[0].querySelector('.popup__save-btn');
 const formProfile = popUpActive[0].querySelector('.profile-form');
 
 editProfileBtn.addEventListener("click", function openProfileEdit () {
@@ -20,14 +20,6 @@ const closeInputProfile = closeBtn[0].addEventListener("click", function closePr
   popUpProfileForm.classList.remove("popup_active")
 });
 
-// function handleProfileFormSubmit(evt) {
-//   evt.preventDefault();
-//   pageProfileName.textContent = inputProfileName.value;
-//   pageAboutMe.textContent = inputAboutMe.value;
-//   popUpProfileForm.classList.remove("popup_active")
-// }
-
-// saveBtn.addEventListener('submit', handleProfileFormSubmit);
 formProfile.addEventListener('submit', handleProfileFormSubmit);
 
 function handleProfileFormSubmit(evt){
@@ -36,14 +28,6 @@ function handleProfileFormSubmit(evt){
   pageAboutMe.textContent = inputAboutMe.value;
   popUpProfileForm.classList.remove("popup_active")
 };
-
-// handleProfileFormSubmit();
-
-// saveBtn.addEventListener('click', function editProfile() {
-//   pageProfileName.textContent = inputProfileName.value;
-//   pageAboutMe.textContent = inputAboutMe.value;
-//   PopUpProfileForm.classList.remove("popup_active")
-// });
 
 addBtn.addEventListener("click", function openImageAdd () {
   inputImg.classList.add("popup_active")
