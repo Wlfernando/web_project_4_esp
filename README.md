13908e572835343e4f485a5de8942f0654d1d1ba
Fernando Alcázar Malfavón <4maferal@gmail.com>
2023-06-04 16:16:27 -0600

    feature: modal box for Image form and visualization of image. Bottuns trashes and likes works

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 * Compatibilidad
 * Tecnologías
 * Resumen
+* Actualización
 * Link
 
 ### Introducción
@@ -36,6 +37,10 @@ Proyecto con enfoque interactivo, en pleno desarrollo. Muestra los lugares visit
 ### Resumen
 
 Las imágenes jpg fueron optimizadas empleando x2 el tamaño de pixeles de las mismas, todos los íconos son imágenes vectoriales, solo se está empleando las fuentes necesarias para el proyecto y se busca crecer las funcionalidades de la pagina y conectar la pagina con un servidor para más adelante guardar datos.
+
+### Actualización
+
+Se agregó un formulario para agregar una tarjeta a la página. Se le agregó funcionalidad al botón de me gusta (corazón) dentro de las tarjetas que permite registrar la acción en eventos; así como al ícono de basura que se encuentra en la esquina superior derecha de las tarjetas que nos permite borrar la tarjeta correspondiente. Podemos darle click a la imagen de cada tarjeta para visualizarla en una ventana modal. Las transiciones de cada ventana modal son fluidas.
 
 ### Link
 
--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,8 @@ const profile = document.querySelector(".profile");
 const editProfileBtn = profile.querySelector(".profile__edit-button");
 const addBtn = profile.querySelector(".profile__add-button");
 
+const cardsContainer = document.querySelector('.cards');
+
 // Pop ups
 const popUpActive = profile.querySelectorAll(".popup");
 const popUpProfileForm = popUpActive[0];
@@ -17,32 +19,8 @@ const inputAboutMe = popUpActive[0].querySelector('[name="aboutMe"]');
 const pageAboutMe = profile.querySelector('.profile__about-me');
 const saveBtn = popUpActive[0].querySelector('.popup__save-btn');
 
-const cards = [
-  {
-  name: "Valle de Yosemite",
-  link: "../images/Card/Yosemite's-Valley.jpg"
-},
-{
-  name: "Lago Louise",
-  link: "../images/Card/Louise's-lake.jpg"
-},
-{
-  name: "Montañas Calvas",
-  link: "../images/Card/bald-mountains.jpg"
-},
-{
-  name: "Latemar",
-  link: "../images/Card/Latemar.jpg"
-},
-{
-  name: "Parque Nacional de la Vanoise",
-  link: "../images/Card/Vanois-National.jpg"
-},
-{
-  name: "Lago di Braies",
-  link: "../images/Card/Lago-di-Braies.jpg"
-}
-];
+const formImage = popUpActive[1].querySelector('.image-form');
+const displayImage = popUpActive[2];
 
 editProfileBtn.addEventListener("click", function openProfileEdit () {
   popUpProfileForm.classList.add("popup_active")
@@ -58,10 +36,68 @@ function handleProfileFormSubmit(evt){
   evt.preventDefault();
   pageProfileName.textContent = inputProfileName.value;
   pageAboutMe.textContent = inputAboutMe.value;
-  popUpProfileForm.classList.remove("popup_active")
+  // popUpProfileForm.classList.remove("popup_active")
 };
 
-addBtn.addEventListener("click", function openImageAdd () {
+const defaultCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg"
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg"
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg"
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg"
+}
+];
+
+function renderCards (name, link) {
+  const cardTemplate = document.querySelector('#cards').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const rmBtn = cardElement.querySelector('.card__trash-button');
+  const likeBtn = cardElement.querySelector('.card__like-button');
+  
+  cardImage.src = link;
+  cardImage.alt = name;
+  cardElement.querySelector('.card__place-name').textContent = name;
+  
+  cardsContainer.prepend(cardElement);
+
+  cardImage.addEventListener('click', () => { 
+    displayImage.querySelector('.popup__title-image').textContent = name;
+    displayImage.querySelector('.popup__image').src = link;
+    displayImage.classList.add("popup_active");
+});
+
+  likeBtn.addEventListener('click', evt =>
+    evt.target.classList.toggle('card__like-button_active')
+  );
+
+  rmBtn.addEventListener('click', () => {
+    const card = rmBtn.closest('.card');
+    card.remove();
+  });
+};
+
+defaultCards.forEach(card => renderCards(card.name, card.link));
+
+addBtn.addEventListener("click", function openImageForm () {
   inputImg.classList.add("popup_active")
 });
 
@@ -69,20 +105,19 @@ const closeInputImage = closeBtn[1].addEventListener("click", function closeImag
   inputImg.classList.remove("popup_active")
 });
 
-const formImage = popUpActive[1].querySelector('.image-form');
-const displayImage = popUpActive[2];
 
 formImage.addEventListener('submit', handleImageFormSubmit);
 
 function handleImageFormSubmit(evt){
   evt.preventDefault();
-  let inputImageTitle = document.querySelector('[name="imageName"]').value;
-  let inputImageSrc = document.querySelector('[name="imageSrc"]').value;
-  let popUpImageContent = profile.querySelector('.popup__image-container');
-  popUpImageContent.querySelector('.popup__title-image').textContent = inputImageTitle;
-  popUpImageContent.querySelector('.popup__image').src = inputImageSrc;
-  inputImg.classList.remove("popup_active");
-  displayImage.classList.add("popup_active");
+  let inputImageTitle = document.querySelector('[name="imageName"]');
+  let inputImageSrc = document.querySelector('[name="imageSrc"]');
+
+  renderCards(inputImageTitle.value, inputImageSrc.value);
+
+  inputImageTitle.value = '';
+  inputImageSrc.value = '';
+  // inputImg.classList.remove("popup_active");
 };
 
 const closePopUpImage = closeBtn[2].addEventListener("click", function closeImageAdd () {
