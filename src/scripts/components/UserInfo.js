702151b02b06ab8f5c5c840c095aaa28a3406f64
Fernando Alcázar Malfavón <4maferal@gmail.com>
2023-09-26 17:51:14 -0600

    Refactor: add properties

--- src/scripts/components/UserInfo.js
+++ src/scripts/components/UserInfo.js
@@ -1,12 +1,20 @@
 export default class UserInfo {
-  constructor({name, about, avatar}) {
+  constructor({name, about, avatar, _id, cohort}) {
     this._name = name;
-    this._job = about
-    this._avatar = avatar
+    this._job = about;
+    this._avatar = avatar;
+    this._id = _id;
+    this._group = cohort
   }
 
   getUserInfo() {
-    return {name: this._name, about: this._job, avatar: this._avatar}
+    return {
+      name: this._name,
+      about: this._job,
+      avatar: this._avatar,
+      id: this._id,
+      group: this._group
+    }
   }
 
   setUserInfo() {
